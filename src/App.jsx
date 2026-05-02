d022da7bb2c93ac28ba4b9aed8447c512219ac22
Kaodinna <chijiokenwoye64@gmail.com>
2026-05-02 22:57:07 +0100

    ygfy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CredentialTracker from "./pages/CredentialTracker";
 import EmergencyContact from "./pages/EmergencyContact";
 import DCFChecklist from "./pages/DCFChecklist";
+import EnrollmentChecklist from "./pages/EnrollmentChecklist";
+import ParentAuth from "./pages/ParentAuth";
 export default function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/credential-tracker" element={<CredentialTracker />} />
         <Route path="/emergency-contact" element={<EmergencyContact />} />
         <Route path="/dcf-checklist" element={<DCFChecklist />} />
+        <Route path="/enrollment-checklist" element={<EnrollmentChecklist />} />
+        <Route path="/parent-auth" element={<ParentAuth />} />
       </Routes>
     </BrowserRouter>
   );
